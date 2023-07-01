1e61bdcef59d7a91a266ee86c12eab1d330f9782
Nadezda <nadborgod@mail.ru>
2023-07-01 13:02:18 +0300

    Исправила решение

--- Program.cs
+++ Program.cs
@@ -6,56 +6,34 @@
 		// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 		// [“Russia”, “Denmark”, “Kazan”] → []
 		
-		string[] arr1 = {"Hello", "2", "world", ":-)"};
-		string[] arr2 = {"1234", "1567", "-2", "computer science"};
-		string[] arr3 = {"Russia", "Denmark", "Kazan"};
+		string[] array = new string [4] {"1234", "1567", "-2", "computer science"};
+		string[] array1 = new string[array.Length];
 		
-		int max = 3;
 		
-		string[] GetShortWordsArray(string[] array)
+		void ThreeSymbolsArray(string[] array, string[] array1)
 		{
-			int length = array.Length;			
-			string[] result = new string[length];
 			int count = 0;
-			
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < array.Length; i++)
 			{
-				if(array[i].Length <= max)
+				if(array[i].Length <= 3)
 				{
-					result[count] = array[i];
+					array1[count] = array[i];
 					count++;
 				}
 			}
-			Array.Resize(ref result, count);
-			
-			return result;
 		}
 		
 		void PrintArray(string[] array)
 		{
-			if(array.Length == 0)
-			{
-				Console.WriteLine("Array is empty");
-			}
-			else
-			{
-				Array.ForEach(array, (str) => Console.Write($"{str} "));
-				Console.WriteLine();
-			}
-		}
-		
-		void PrintTask(string[] array)
-		{
-			Console.WriteLine("Source array:");
-			PrintArray(array);
-			string[] shortArray = GetShortWordsArray(array);
-			Console.WriteLine($"There are words less or equal than {max}:");
-			PrintArray(shortArray);
-			Console.WriteLine();
-		}
-		
-		PrintTask(arr1);
-		PrintTask(arr2);
-		PrintTask(arr3);
+    	for (int i = 0; i < array.Length; i++)
+    	{
+        Console.Write($"{array[i]} ");
+    	}
+    	Console.WriteLine();
+}
+		ThreeSymbolsArray(array, array1);
+		PrintArray(array1);
+
+
 
 
